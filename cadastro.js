886f954b9f17bb8c3e6d1eb954b65e54cdb43b04
William Gonçalves <williamlion04@gmail.com>
2022-10-06 01:06:38 -0300

    Initial commit

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+function confirmar() {
+    var senha = document.getElementById('senha')
+    var confirmar = document.getElementById('conf-senha')
+    
+    var Senha = Number(senha.value)
+    var Confirmar = Number(confirmar.value)
+    var versenha = document.getElementById('Msenha')
+
+    
+}
